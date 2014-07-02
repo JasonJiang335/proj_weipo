26f42062128383eb2463d6f2d83c8378d6e23770
JasonJiang335 <jiangzidong01@gmail.com>
2014-07-02 10:38:21 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,40 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+# ORIGINAL  ProjWeipo::Application.config.secret_key_base = 'e8c12d3cf9209f50ac4d42d7dce93a3d5957bdacde5f549bd0ade0c74edb95ab0791522161c75117eaefe09d084bae02e0f26ccf587e12a004c193bbcb1e2b9e'
+# -------------------------------------------------------------
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+ProjWeipo::Application.config.secret_key_base = secure_token
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjWeipo::Application.config.session_store :cookie_store, key: '_proj_weipo_session'
